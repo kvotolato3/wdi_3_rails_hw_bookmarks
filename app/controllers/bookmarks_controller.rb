d0881dd728c7d2da520a4f11ee26d58545caab77
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-08 19:11:03 -0400

    Add links to individual bookmarks and new bookmark link

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,4 +2,23 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks= Bookmark.order(:title)
   end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    bookmark = Bookmark.create(bookmark_params)
+    redirect_to bookmark
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :category, :comment, :url, :is_favorite)
+  end
 end
